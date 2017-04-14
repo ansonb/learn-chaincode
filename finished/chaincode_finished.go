b1b38a29cf783740d40bb4549b4619b6f827abfd
ansonb <ansonbastos@gmail.com>
2017-04-14 15:27:41 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -97,7 +97,7 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 
 // read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	/*var key, jsonResp string
+	var key, jsonResp string
 	var err error
 
 	if len(args) != 1 {
@@ -110,7 +110,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-        */
-	var newByte = []byte("Test change....")
-	return newByte, nil
+        
+	//var newByte = []byte("Test change....")
+	return valAsbytes, nil
 }
