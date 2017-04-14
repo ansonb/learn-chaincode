ffa6a7377942988e49b96a033b6c7fee6e1a75ad
ansonb <ansonbastos@gmail.com>
2017-04-14 15:24:36 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -110,7 +110,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-        
-	var newByte = []byte("Test change....")*/
+        */
+	var newByte = []byte("Test change....")
 	return newByte, nil
 }
