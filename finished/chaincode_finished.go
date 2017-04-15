181509a105a09e93b88ff9a6b4c1aab7fbc2fe9a
ansonb <ansonbastos@gmail.com>
2017-04-15 09:16:42 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -19,12 +19,78 @@ package main
 import (
 	"errors"
 	"fmt"
-
+        "encoding/json"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
+}
+
+var logger = shim.NewLogger("CLDChaincode")
+
+//==============================================================================================================================
+//	 Participant types - Each participant type is mapped to an integer which we use to compare to the value stored in a
+//						 user's eCert
+//==============================================================================================================================
+//CURRENT WORKAROUND USES ROLES CHANGE WHEN OWN USERS CAN BE CREATED SO THAT IT READ 1, 2, 3, 4, 5
+const   AUTHORITY          =  "regulator"
+const   BORROWER   	   =  "borrower"
+const   LEADARRANGER	   =  "leadarranger"
+const   PARTICIPATINGBANK  =  "participatingbank"
+
+
+//==============================================================================================================================
+//	 Status types - Asset lifecycle is broken down into 6 statuses, this is part of the business logic to determine what can
+//					be done to the loan at points in it's lifecycle
+//==============================================================================================================================
+const   STATE_INIT			=  0
+const   STATE_LA_ACCEPT  		=  1
+const   STATE_INVITE_PARTICIPATING_BANK	=  2
+const   STATE_PARTICIPATING_BANK_ACCEPT	=  3
+const   STATE_DISBURSED  		=  4
+const   STATE_REPAYED			=  5
+//==============================================================================================================================
+//	 Structure Definitions
+//==============================================================================================================================
+//	Chaincode - A blank struct for use with Shim (A HyperLedger included go file used for get/put state
+//				and other HyperLedger functions)
+//==============================================================================================================================
+type  SimpleChaincode struct {
+}
+
+//==============================================================================================================================
+//	Vehicle - Defines the structure for a car object. JSON on right tells it what JSON fields to map to
+//			  that element when reading a JSON object into the struct e.g. JSON make -> Struct Make.
+//==============================================================================================================================
+type loan struct {
+	loanAmount            string `json:"loanAmount"`
+	disbursedAmoun        int `json:"disbursedAmount"`
+	repayedAmount         int `json:"repayedAmount"`
+	borrower              string `json:"borrower"`
+	leadArranger          string `json:"leadArranger"`
+	participatingBank     bool `json:"participatingBank"`
+	Status                int `json:"status"`
+	V5cID                 string `json:"v5cID"`
+}
+
+
+//==============================================================================================================================
+//	V5C Holder - Defines the structure that holds all the v5cIDs for contracts that have been created.
+//				Used as an index when querying all loan contracts.
+//==============================================================================================================================
+
+type V5C_Holder struct {
+	V5Cs 	[]string `json:"v5cs"`
+}
+
+//==============================================================================================================================
+//	User_and_eCert - Struct for storing the JSON of a user and their ecert
+//==============================================================================================================================
+
+type User_and_eCert struct {
+	Identity string `json:"identity"`
+	eCert string `json:"ecert"`
 }
 
 func main() {
@@ -36,7 +102,7 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
+	/*if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
 
@@ -45,10 +111,128 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 		return nil, err
 	}
 
+	return nil, nil*/
+		//Args
+	//				0
+	//			peer_address
+
+	var v5cIDs V5C_Holder
+
+	bytes, err := json.Marshal(v5cIDs)
+
+        if err != nil { return nil, errors.New("Error creating V5C_Holder record") }
+
+	err = stub.PutState("v5cIDs", bytes)
+
+	for i:=0; i < len(args); i=i+2 {
+		t.add_ecert(stub, args[i], args[i+1])
+	}
+
 	return nil, nil
 }
 
-// Invoke isur entry point to invoke a chaincode function
+//==============================================================================================================================
+//	 add_ecert - Adds a new ecert and user pair to the table of ecerts
+//==============================================================================================================================
+
+func (t *SimpleChaincode) add_ecert(stub shim.ChaincodeStubInterface, name string, ecert string) ([]byte, error) {
+
+
+	err := stub.PutState(name, []byte(ecert))
+
+	if err == nil {
+		return nil, errors.New("Error storing eCert for user " + name + " identity: " + ecert)
+	}
+
+	return nil, nil
+
+}
+
+
+//==============================================================================================================================
+//	 get_caller - Retrieves the username of the user who invoked the chaincode.
+//				  Returns the username as a string.
+//==============================================================================================================================
+
+func (t *SimpleChaincode) get_username(stub shim.ChaincodeStubInterface) (string, error) {
+
+    username, err := stub.ReadCertAttribute("username");
+	if err != nil { return "", errors.New("Couldn't get attribute 'username'. Error: " + err.Error()) }
+	return string(username), nil
+}
+
+//==============================================================================================================================
+//	 check_affiliation - Takes an ecert as a string, decodes it to remove html encoding then parses it and checks the
+// 				  		certificates common name. The affiliation is stored as part of the common name.
+//==============================================================================================================================
+
+func (t *SimpleChaincode) check_affiliation(stub shim.ChaincodeStubInterface) (string, error) {
+    affiliation, err := stub.ReadCertAttribute("role");
+	if err != nil { return "", errors.New("Couldn't get attribute 'role'. Error: " + err.Error()) }
+	return string(affiliation), nil
+
+}
+
+//==============================================================================================================================
+//	 get_caller_data - Calls the get_ecert and check_role functions and returns the ecert and role for the
+//					 name passed.
+//==============================================================================================================================
+
+func (t *SimpleChaincode) get_caller_data(stub shim.ChaincodeStubInterface) (string, string, error){
+
+	user, err := t.get_username(stub)
+
+    // if err != nil { return "", "", err }
+
+	// ecert, err := t.get_ecert(stub, user);
+
+    // if err != nil { return "", "", err }
+
+	affiliation, err := t.check_affiliation(stub);
+
+    if err != nil { return "", "", err }
+
+	return user, affiliation, nil
+}
+
+//==============================================================================================================================
+//	 retrieve_v5c - Gets the state of the data at v5cID in the ledger then converts it from the stored
+//					JSON into the loan struct for use in the contract. Returns the loan struct.
+//					Returns empty v if it errors.
+//==============================================================================================================================
+func (t *SimpleChaincode) retrieve_v5c(stub shim.ChaincodeStubInterface, v5cID string) (Vehicle, error) {
+
+	var v loan
+
+	bytes, err := stub.GetState(v5cID);
+
+	if err != nil {	fmt.Printf("RETRIEVE_V5C: Failed to invoke vehicle_code: %s", err); return v, errors.New("RETRIEVE_V5C: Error retrieving vehicle with v5cID = " + v5cID) }
+
+	err = json.Unmarshal(bytes, &v);
+
+    if err != nil {	fmt.Printf("RETRIEVE_V5C: Corrupt vehicle record "+string(bytes)+": %s", err); return v, errors.New("RETRIEVE_V5C: Corrupt vehicle record"+string(bytes))	}
+
+	return v, nil
+}
+
+//==============================================================================================================================
+// save_changes - Writes to the ledger the Vehicle struct passed in a JSON format. Uses the shim file's
+//				  method 'PutState'.
+//==============================================================================================================================
+func (t *SimpleChaincode) save_changes(stub shim.ChaincodeStubInterface, v loan) (bool, error) {
+
+	bytes, err := json.Marshal(v)
+
+	if err != nil { fmt.Printf("SAVE_CHANGES: Error converting loan record: %s", err); return false, errors.New("Error converting loan record") }
+
+	err = stub.PutState(v.V5cID, bytes)
+
+	if err != nil { fmt.Printf("SAVE_CHANGES: Error storing loan record: %s", err); return false, errors.New("Error storing loan record") }
+
+	return true, nil
+}
+
+// Invoke issuer entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("invoke is running " + function)
 
